2e3d2dab47c3f7ee283338c1b631508114e27232
Dave North <dnorth@signiant.com>
2018-10-11 19:57:28 -0400

    filter VMs and call stub snapshot function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # vmware-snapshot-taker
 Triggers a snapshot for VMs listed in a file
+
+Based on a couple of the samples in the pyvomi repo https://github.com/vmware/pyvmomi-community-samples
---
+++ sig-vmware-snapshot-taker.py
@@ -0,0 +1,122 @@
+"""
+Python program for listing the vms on an ESX / vCenter host
+"""
+
+import atexit
+
+from pyVim import connect
+from pyVmomi import vmodl
+from pyVmomi import vim
+
+import tools.cli as cli
+
+import json
+
+def get_vm_info(virtual_machine):
+    summary = virtual_machine.summary
+
+    return summary
+
+def print_vm_info(virtual_machine):
+    """
+    Print information for a particular virtual machine or recurse into a
+    folder with depth protection
+    """
+    summary = virtual_machine.summary
+    print("Name       : ", summary.config.name)
+    print("Template   : ", summary.config.template)
+    print("Path       : ", summary.config.vmPathName)
+    print("Guest      : ", summary.config.guestFullName)
+    print("Instance UUID : ", summary.config.instanceUuid)
+    print("Bios UUID     : ", summary.config.uuid)
+    annotation = summary.config.annotation
+    if annotation:
+        print("Annotation : ", annotation)
+    print("State      : ", summary.runtime.powerState)
+    if summary.guest is not None:
+        ip_address = summary.guest.ipAddress
+        tools_version = summary.guest.toolsStatus
+        if tools_version is not None:
+            print("VMware-tools: ", tools_version)
+        else:
+            print("Vmware-tools: None")
+        if ip_address:
+            print("IP         : ", ip_address)
+        else:
+            print("IP         : None")
+    if summary.runtime.question is not None:
+        print("Question  : ", summary.runtime.question.text)
+    print("")
+
+def create_snapshot(si, uuid):
+    instance_search = True  # we're using instance UUIDs
+    vm = si.content.searchIndex.FindByUuid(None, uuid, True, instance_search)
+
+    if vm is None:
+        return False
+    else:
+        return True
+
+def main():
+    """
+    Simple command-line program for listing the virtual machines on a system.
+    """
+
+    vm_names_to_snap = []
+
+    parser = cli.build_arg_parser()
+    parser.add_argument('-f', '--file', required=True,
+                        help="File containing a list of VMs to snapshot.")
+    args = parser.parse_args()
+
+    # Grab the file containing the VMs to snapshot
+    with open(args.file, 'r') as f:
+        vms_to_snap = json.load(f)
+
+    for snapshot_vm in vms_to_snap:
+        vm_names_to_snap.append(snapshot_vm['Name'])
+
+    try:
+        if args.disable_ssl_verification:
+            service_instance = connect.SmartConnectNoSSL(host=args.host,
+                                                         user=args.user,
+                                                         pwd=args.password,
+                                                         port=int(args.port))
+        else:
+            service_instance = connect.SmartConnect(host=args.host,
+                                                    user=args.user,
+                                                    pwd=args.password,
+                                                    port=int(args.port))
+
+        atexit.register(connect.Disconnect, service_instance)
+
+        content = service_instance.RetrieveContent()
+
+        container = content.rootFolder  # starting point to look into
+        viewType = [vim.VirtualMachine]  # object types to look for
+        recursive = True  # whether we should look into it recursively
+        containerView = content.viewManager.CreateContainerView(
+            container, viewType, recursive)
+
+        children = containerView.view
+        for child in children:
+            info = get_vm_info(child)
+
+            if info.config.name in vm_names_to_snap:
+                uuid = info.config.instanceUuid
+
+                if create_snapshot(service_instance, uuid):
+                    print "Snapshot triggered for " + info.config.name
+                else:
+                    print "Error trigging snapshot for " + info.config.name
+                #print_vm_info(child)
+
+    except vmodl.MethodFault as error:
+        print("Caught vmodl fault : " + error.msg)
+        return -1
+
+    return 0
+
+# Start program
+if __name__ == "__main__":
+    main()
